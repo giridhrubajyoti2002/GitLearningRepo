f099551bc63ab04731fc01c3d0e18a7d2e38c727
Dhrubajyoti Giri <giridhrubajyoti2002@gmail.com>
2020-12-19 23:18:42 +0530

    abcd

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int n;
+    cin>>n;
+
+    cout<<"Hello World"<<endl;
+    if(n%2==0){
+        cout<<"Even Number"<<endl;
+    }
+
+
+
+
+
+    return 0;
+}
